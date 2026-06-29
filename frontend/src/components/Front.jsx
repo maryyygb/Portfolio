2b54c9f0e94c4cff1acde21d43fa2139b3f0c199
maryyygb <marygbasilio8@gmail.com>
2026-06-29 17:06:01 +0800

    Frontend: Done with the first part of the frontpage.

--- frontend/src/components/Front.jsx
+++ frontend/src/components/Front.jsx
@@ -1,41 +1,54 @@
 // import React from "react";
+import { TfiDownload, TfiArrowRight } from "react-icons/tfi";
 
 const Front = () => {
   return (
-    <div className="z-20 front-bg flex items-center h-[92vh] w-full p-5 sm:p-25">
+    <div className="hero z-20 front-bg flex items-center h-[92vh] w-full p-5 sm:p-25 lg:pl-35">
       <div className="deco-sm hidden sm:block"></div>
+      <div className="deco-sm2 hidden sm:block"></div>
       <div className="flex flex-col gap-2">
-        <div className="dark-bg text-center text-[8px] tracking-widest rounded-[2rem] pt-2 pb-2 pl-5 pr-5 w-[45%] w-fit sm:text-xl flex justify-center items-center sm:gap-2">
+        <div className="dark-bg text-center text-[8px] tracking-widest rounded-[2rem] py-2 px-5 w-[45%] w-fit sm:text-sm lg:text-xl flex justify-center items-center sm:gap-1 md:gap-2">
           <div className="active-dot-mobile sm:w-2 sm:h-2 text-center sm:mt-[1px] mr-1"></div>
           AVAILABLE FOR WORK
         </div>
-        <h1 className="role text-5xl font-bold sm:text-9xl sm:w-[60%]">
-          Junior Web <span className="text-amber-600/60 italic">Developer</span>{" "}
-          & Support
+        <h1 className="role text-5xl font-bold sm:text-7xl lg:text-9xl lg:w-[60%] w-[90%]">
+          Junior Web{" "}
+          <span className="text-orange-500/60 italic">Developer</span> & Support
         </h1>
-        <p className="text-white/50 sm:text-3xl mt-3 sm:mt-6 sm:w-[80%]">
+        <p className="text-orange-50/50 sm:text-2xl md:text-xl lg:text-3xl mt-3 sm:mt-6 sm:w-[80%]">
           Passionate about creating{" "}
-          <span className="text-white">user-friendly web solutions</span> and
+          <span className="text-orange-50">user-friendly web solutions</span> and
           delivering{" "}
-          <span className="text-white">reliable backend support</span> that
+          <span className="text-orange-50">reliable backend support</span> that
           keeps businesses running smoothly.
         </p>
-        <div
-          className="text-white flex flex-row gap-2 sm:gap-5 sm:text-2xl
-         sm:mt-6 mt-1"
-        >
+        <div className="text-orange-50 text-sm flex flex-row gap-2 sm:gap-5 sm:text-2xl sm:mt-6 mt-4">
           <a
             href=""
-            className="or-grad pr-6 pl-6 pt-2 pb-2 bg-amber-500 rounded-full border-solid border-1 border-slate-500/80 border-dark sm:pl-14 sm:pr-14 sm:pt-4 sm:pb-4 sm:text-3xl text-black font-medium"
+            className="or-grad px-6 py-2 bg-amber-500 rounded-full border-solid border-1 border-slate-500/80 border-dark sm:px-14 sm:py-4 sm:text-xl lg:text-3xl font-medium flex items-center gap-4"
           >
-            See My Work →
+            See My Work <TfiArrowRight />
           </a>
           <a
             href=""
-            className="pr-6 pl-6 pt-2 pb-2 sm:pl-14 sm:pr-14 sm:pt-4 sm:pb-4 sm:text-3xl text-white/80 rounded-full border-solid border-1 border-white/25"
+            className="pr-6 pl-6 pt-2 pb-2 sm:px-14 sm:py-4 sm:text-xl lg:text-3xl text-orange-50/80 rounded-full border-solid border-1 border-orange-50/25 z-40 flex gap-4 items-center"
           >
-            Let's Talk
+            Download CV <TfiDownload />
           </a>
+        </div>
+        <div className="experience-sec flex text-orange-50 my-6 md:my-10 lg:my-18 md:gap-10 gap-4">
+          <div>
+            <h3 className="exp-num text-3xl font-bold text-orange-500/90 lg:text-6xl md:text-4xl">1+</h3>
+            <p className="text-xs text-orange-50/50 tracking-wide md:text-base lg:text-xl md:tracking-normal md:my-2 md:font-semibold">YEARS EXPERIENCE</p>
+          </div>
+          <div>
+            <h3 className="exp-num text-3xl font-bold text-orange-500/90 lg:text-6xl md:text-4xl">6+</h3>
+            <p className="text-xs text-orange-50/50 tracking-wide md:text-base lg:text-xl md:tracking-normal md:my-2 md:font-semibold">PERSONAL PPROJECTS</p>
+          </div>
+          <div>
+            <h3 className="exp-num text-3xl font-bold text-orange-500/90 lg:text-6xl md:text-4xl">96%</h3>
+            <p className="text-xs text-orange-50/50 tracking-wide md:text-base lg:text-xl md:tracking-normal md:my-2 md:font-semibold">CLIENT SATISFACTION</p>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react';
+import { TiThMenu } from "react-icons/ti";
 
 const Navbar = () => {
 
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="navbar h-[8vh] w-[100%] bg-[#1C0F06]/98">
+    <div className="navbar h-[8vh] w-[100%] bg-[#1C0F06]/98 border-1 border-b-amber-700/30">
         <div className="h-full w-full flex flex-row text-white justify-center items-center gap-6 sm:pl-12 sm:pr-12 pl-5 pr-5 text-[10px] sm:text-xl">
-            <h1 className='name-logo mr-auto text-[.75rem] sm:text-2xl'>MARY G.B.</h1>
+            <h1 className="name-logo mr-auto text-xl sm:text-2xl lg:text-3xl">MARY G.B.</h1>
 
               {/* DESKTOP RESPONSIVENESS */}
-            <div className="nav-list-con hidden sm:flex gap-6">
-                <p className='cursor-pointer'><a href="#">ABOUT</a></p>
-                <p className='cursor-pointer'><a href="#">SKILLS</a></p>
-                <p className='cursor-pointer'><a href="#">WORKS</a></p>
-                <p className='cursor-pointer'><a href="#">REVIEWS</a></p>
-                <p className='cursor-pointer'><a href="#">HIRE ME</a></p>
+            <div className="nav-list-con font-semibold hidden text-orange-50 sm:flex gap-8 sm:items-center sm:text-base md:text-xl lg:text-2xl">
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">ABOUT</a></p>
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">SKILLS</a></p>
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">WORKS</a></p>
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">REVIEWS</a></p>
+                <p className="cursor-pointer or-grad px-8 py-3 rounded-full w-[30%] text-center font-medium"><a href="#">HIRE ME</a></p>
             </div>
 
-            <button className='cursor-pointer sm:hidden'
-                    onClick={() => setOpen(!open)}>=</button>
+            <button className='cursor-pointer text-lg text-orange-400/70 sm:hidden'
+                    onClick={() => setOpen(!open)}><TiThMenu /></button>
         </div>
         {/* MOBILE RESPONSIVENESS */}
         {open && (
-          <div className="sm:hidden nav-list-con text-[10px] flex flex-col items-center gap-6 bg-[#1C0F06] p-4 text-white mt-0">
-                <p className='cursor-pointer'><a href="#">ABOUT</a></p>
-                <p className='cursor-pointer'><a href="#">SKILLS</a></p>
-                <p className='cursor-pointer'><a href="#">WORKS</a></p>
-                <p className='cursor-pointer'><a href="#">REVIEWS</a></p>
+          <div className="sm:hidden font-semibold text-orange-50 nav-list-con text-xs flex flex-col items-center gap-6 bg-[#1C0F06] p-4 text-white mt-0 mb-4">
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">ABOUT</a></p>
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">SKILLS</a></p>
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">WORKS</a></p>
+                <p className='cursor-pointer hover:underline-orange-500/50 hover:underline-offset-2'><a href="#">REVIEWS</a></p>
                 <p className='cursor-pointer or-grad pt-2 pb-2 rounded-[1rem] w-[30%] text-center'><a href="#" className='w-full '>HIRE ME</a></p>
             </div>
         )}
